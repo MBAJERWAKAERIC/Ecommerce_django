d1397c7c8d0589430ccbc1462285a76df4b9e15d
mbajerwakaeric <erickmbanjerwaka@gmail.com>
2023-01-05 21:58:53 +0300

    prod super user

--- ec/app/admin.py
+++ ec/app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . models import Product
 
 # Register your models here.
+
+
+@admin.register(Product)
+class ProductModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'discouted_price', 'category' 'product_image']
--- ec/app/models.py
+++ ec/app/models.py
@@ -13,11 +13,11 @@ CATEGORY_CHOICES = (
     ('IC', 'Ice-Creams'),
     )
 
-class product(models.model):
+class Product(models.Model):
     title = models.CharField(max_length=100)
     selling_price = models.FloatField()
     discounted_price = models.FloatField()
-    description = models.textField()
+    description = models.TextField()
     composition = models.TextField(default='')
     prodapp = models.TextField(default='')
     category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
