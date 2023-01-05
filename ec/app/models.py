0d71c8563f9ab358445be9f10446be949aebaef1
mbajerwakaeric <erickmbanjerwaka@gmail.com>
2023-01-05 21:38:29 +0300

    media and models

--- ec/app/models.py
+++ ec/app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+CATEGORY_CHOICES = (
+    ('CR', 'Curd'),
+    ('ML', 'Milk'),
+    ('LS', 'Lassi'),
+    ('MS', 'MilkShake'),
+    ('PN', 'Paneer'),
+    ('GH', 'Ghee'),
+    ('CZ', 'Cheese'),
+    ('IC', 'Ice-Creams'),
+    )
+
+class product(models.model):
+    title = models.CharField(max_length=100)
+    selling_price = models.FloatField()
+    discounted_price = models.FloatField()
+    description = models.textField()
+    composition = models.TextField(default='')
+    prodapp = models.TextField(default='')
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
+    product_image = models.ImageField(upload_to='product')
+    def _str_(self):
+        return self.title
