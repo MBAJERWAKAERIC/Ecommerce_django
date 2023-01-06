87d23d4b98b89869e8b4901a01dbb71e7669e00f
mbajerwakaeric <erickmbanjerwaka@gmail.com>
2023-01-06 12:52:37 +0300

    category page update

--- ec/app/urls.py
+++ ec/app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from  . import views
+
 urlpatterns = [
     path("", views.home),
+    path("category/", views.CategoryView.as_view(),name="category"),
 ]
--- ec/app/views.py
+++ ec/app/views.py
@@ -1,5 +1,12 @@
+from urllib import request
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
 def home(request):
     return render(request, 'app/home.html')
+
+class CategoryView(View):
+    def get(self, request):
+        return render(request, 'app/category.html')
