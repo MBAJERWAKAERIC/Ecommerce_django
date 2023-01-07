f9b7f54569ec01fd17c73f40b9712303d9906d60
mbajerwakaeric <erickmbanjerwaka@gmail.com>
2023-01-07 21:45:38 +0300

    home corrrection

--- ec/app/urls.py
+++ ec/app/urls.py
@@ -6,5 +6,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("", views.home),
     path("category/<slug:val>", views.CategoryView.as_view(),name="category"),
+    path("category-title/<val>", views.CategoryTitle.as_view(),name="category-title"),
     path("product-detail/<int:pk>", views.ProductDetail.as_view(),name="product-detail"),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- ec/app/views.py
+++ ec/app/views.py
@@ -17,7 +17,7 @@ class CategoryView(View):
 class CategoryTitle(View):
     def get(self, request,val):
         product = Product.objects.filter(title=val)
-        title = Product.objects.filter(category=product[0].category).values('title').annotate(total=Count('title'))
+        title = Product.objects.filter(category=product[0].category).values('title')
         return render(request, 'app/category.html', locals())
 
 class ProductDetail(View):
