b5e82f19466a42240f5a0c5781a4417d3e157fbc
mbajerwakaeric <erickmbanjerwaka@gmail.com>
2023-01-07 21:04:44 +0300

    btns

--- ec/app/views.py
+++ ec/app/views.py
@@ -10,8 +10,14 @@ def home(request):
 class CategoryView(View):
     def get(self, request,val):
         product = Product.objects.filter(category=val)
-        title = Product.objects.filter(category=val).values('title').annotate(total=Count('title'))
-        
+        title = Product.objects.filter(category=val).values('title') 
+        return render(request, 'app/category.html', locals())
+
+
+class CategoryTitle(View):
+    def get(self, request,val):
+        product = Product.objects.filter(title=val)
+        title = Product.objects.filter(category=product[0].category).values('title').annotate(total=Count('title'))
         return render(request, 'app/category.html', locals())
 
 class ProductDetail(View):
