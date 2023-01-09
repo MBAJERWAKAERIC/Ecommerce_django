1f740063c1612e94f9a6804e2b22ca83cc461bdc
mbajerwakaeric <erickmbanjerwaka@gmail.com>
2023-01-09 17:11:25 +0300

    CustomerRegistrationForm

---
+++ ec/app/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+
+class CustomerRegistratioForm(UserCreationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'autofocus': 'True', 'class': 'form-control'}))
+    email = forms.CharField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    
+                                
+                                
--- ec/app/views.py
+++ ec/app/views.py
@@ -2,6 +2,7 @@ from django.db.models import Count
 from django.shortcuts import render
 from django.views import View
 from . models import Product
+from . forms import CustomerRegistrationForm
 
 # Create your views here.
 def home(request):
@@ -31,3 +32,8 @@ class ProductDetail(View):
     def get(self, request,pk):
         product = Product.objects.get(pk=pk)
         return render(request, 'app/productdetail.html', locals())
+    
+class CustomerRegistrationView(View):
+    def get(self, request):
+        form = CustomerRegistrationForm()
+        return render(request, 'app/customerregistrationform.html', locals())
