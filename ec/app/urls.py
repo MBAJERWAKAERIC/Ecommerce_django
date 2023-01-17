a9dcdc5282142cb0262afc409a335060145bf5b8
mbajerwakaeric <erickmbanjerwaka@gmail.com>
2023-01-17 14:24:16 +0300

    reset password

--- ec/app/urls.py
+++ ec/app/urls.py
@@ -20,12 +20,12 @@ urlpatterns = [
     #login authentication
     path( 'registration/' , views.CustomerRegistrationView.as_view(), name='customerregistration'),
     path('acounts/login/', auth_view.LoginView.as_view(template_name='app/login.html', authentication_form=LoginForm), name='login'),
-    path('password-reset/', auth_view.PasswordResetView.as_view(template_name='app/password_reset.html', form_class=MyPasswordResetForm), name='password_reset'),
+    
     path('passwochange/',auth_view.PasswordChangeView.as_view(template_name='app/changepassword.html', form_class=MyPasswordChangeForm, success_url='/passwordchangedone'), name='passwordchange'),
     path('passwordchangedone/',auth_view.PasswordChangeDoneView.as_view(template_name='app/passwordchangedone.html'), name='passwordchangedone'),
     path('logout/',auth_view.LogoutView.as_view(next_page='login'), name='logout'),
     
-    path('password-reset/',auth_view.PasswordResetView.as_view(template_name='app/password-reset.html', form_class=MyPasswordResetForm), name='password_reset'),
+    path('password-reset/',auth_view.PasswordResetView.as_view(template_name='app/password_reset.html', form_class=MyPasswordResetForm), name='password_reset'),
     
     path('/password-reset/done',auth_view.PasswordResetDoneView.as_view(template_name='app/password_reset_done.html'), name='password_reset_done'),
     
--- ec/ec/settings.py
+++ ec/ec/settings.py
@@ -127,3 +127,5 @@ LOGIN_REDIRECT_URL = '/profile/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACHEND = "django.core.mail.backends.console.EmailBackend"
