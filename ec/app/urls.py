f812e72112219d7264d880c87051bafb6007d273
mbajerwakaeric <erickmbanjerwaka@gmail.com>
2023-01-17 10:11:01 +0300

    password change don resolved

--- ec/app/urls.py
+++ ec/app/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
     path('acounts/login/', auth_view.LoginView.as_view(template_name='app/login.html', authentication_form=LoginForm), name='login'),
     path('password-reset/', auth_view.PasswordResetView.as_view(template_name='app/password_reset.html', form_class=MyPasswordResetForm), name='password_reset'),
     path('passwochange/',auth_view.PasswordChangeView.as_view(template_name='app/changepassword.html', form_class=MyPasswordChangeForm, success_url='/passwordchangedone'), name='passwordchange'),
-    path('passwochangedone/',auth_view.PasswordChangeDoneView.as_view(template_name='app/passwordchangedone.html'), name='passwordchangedone'),
+    path('passwordchangedone/',auth_view.PasswordChangeDoneView.as_view(template_name='app/passwordchangedone.html'), name='passwordchangedone'),
     
  ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
