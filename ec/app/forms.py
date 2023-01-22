320b194d16028913c155d7d166e1281f1239ed6c
mbajerwakaeric <erickmbanjerwaka@gmail.com>
2023-01-22 20:11:39 +0300

    pws done

--- ec/app/forms.py
+++ ec/app/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UsernameField, PasswordChangeForm, SetPasswordForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UsernameField, PasswordChangeForm, SetPasswordForm, PasswordResetForm
 from django.contrib.auth.models import User
 from .models import Customer
 
@@ -22,7 +22,7 @@ class MyPasswordChangeForm(PasswordChangeForm):
     new_password1 = forms.CharField(label='New Password', widget=forms.PasswordInput(attrs={'autocomplete': 'current-password', 'class':'form-control'}))
     new_password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput(attrs={'autocomplete': 'current-password', 'class': 'form-control'}))
         
-class MyPasswordResetForm(PasswordChangeForm):
+class MyPasswordResetForm(PasswordResetForm):
         email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
     
 class MySetPasswordForm(SetPasswordForm):
