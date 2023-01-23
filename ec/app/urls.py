da46d03e5814243017b1b68327ccc4b224394b4b
mbajerwakaeric <erickmbanjerwaka@gmail.com>
2023-01-23 20:31:02 +0300

    pwd reset

--- ec/app/urls.py
+++ ec/app/urls.py
@@ -23,14 +23,10 @@ urlpatterns = [
     
     path('passwochange/',auth_view.PasswordChangeView.as_view(template_name='app/changepassword.html', form_class=MyPasswordChangeForm, success_url='/passwordchangedone'), name='passwordchange'),
     path('passwordchangedone/',auth_view.PasswordChangeDoneView.as_view(template_name='app/passwordchangedone.html'), name='passwordchangedone'),
-    path('logout/',auth_view.LogoutView.as_view(next_page='login'), name='logout'),
-    
+    path('logout/',auth_view.LogoutView.as_view(next_page='login'), name='logout'),   
     path('password-reset/',auth_view.PasswordResetView.as_view(template_name='app/password_reset.html', form_class=MyPasswordResetForm), name='password_reset'),
-    
     path('password-reset/done/',auth_view.PasswordResetDoneView.as_view(template_name='app/password_reset_done.html'), name='password_reset_done'),
-    
     path('password-reset-confirm/<uidb64>/<token>/',auth_view.PasswordResetConfirmView.as_view(template_name='app/password_reset_confirm.html', form_class=MySetPasswordForm), name='password_reset_confirm'),
-    
     path('password-reset-complete/', auth_view.PasswordResetCompleteView.as_view(template_name='app/password_reset_complete.html'), name='password_reset_complete'),
     
  ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
