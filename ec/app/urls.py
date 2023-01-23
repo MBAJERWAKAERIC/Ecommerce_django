fe90bf977a4ea1f97bb353e80c933fb03b7600ac
mbajerwakaeric <erickmbanjerwaka@gmail.com>
2023-01-23 18:24:34 +0300

    nnn

--- ec/app/urls.py
+++ ec/app/urls.py
@@ -27,9 +27,9 @@ urlpatterns = [
     
     path('password-reset/',auth_view.PasswordResetView.as_view(template_name='app/password_reset.html', form_class=MyPasswordResetForm), name='password_reset'),
     
-    path('/password-reset/done',auth_view.PasswordResetDoneView.as_view(template_name='app/password_reset_done.html'), name='password_reset_done'),
+    path('password-reset/done/',auth_view.PasswordResetDoneView.as_view(template_name='app/password_reset_done.html'), name='password_reset_done'),
     
-    path('/password-reset-confirm/<uidb64>/<token>/',auth_view.PasswordResetConfirmView.as_view(template_name='app/password_reset_confirm.html', form_class=MySetPasswordForm), name='password_reset_confirm'),
+    path('password-reset-confirm/<uidb64>/<token>/',auth_view.PasswordResetConfirmView.as_view(template_name='app/password_reset_confirm.html', form_class=MySetPasswordForm), name='password_reset_confirm'),
     
     path('password-reset-complete/', auth_view.PasswordResetCompleteView.as_view(template_name='app/password_reset_complete.html'), name='password_reset_complete'),
     
--- ec/ec/settings.py
+++ ec/ec/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR/'media'
+
 LOGIN_REDIRECT_URL = '/profile/'
 
 
