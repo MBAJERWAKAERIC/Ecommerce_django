529f659cba1169249fa9c1c127d930278092e58c
mbajerwakaeric <erickmbanjerwaka@gmail.com>
2023-01-30 19:58:39 +0300

    shopping cart

--- ec/app/urls.py
+++ ec/app/urls.py
@@ -19,6 +19,7 @@ urlpatterns = [
     
     path('add-to-cart/', views.add_to_cart, name="add-to-cart"),
     path('cart/', views.show_cart, name="showcart"),
+    path('checkout/', views.show_cart, name="checkout"),
     
     #login authentication
     path( 'registration/' , views.CustomerRegistrationView.as_view(), name='customerregistration'),
--- ec/app/views.py
+++ ec/app/views.py
@@ -98,9 +98,9 @@ class updateAdress(View):
         return redirect("adress")
 
 def add_to_cart(request):
-    user=request.user
-    product_id=request.GET.get('product_id')
-    product_id=Product.objects.get(id=product_id)
+    user = request.user
+    product_id = request.GET.get('prod_id')
+    product = Product.objects.get(id=product_id)
     Cart(user=user, product=product).save()
     return redirect('/cart')
 
