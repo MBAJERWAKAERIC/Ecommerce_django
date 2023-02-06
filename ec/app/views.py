6f0c341e41bf40b2c6fdc60ceff15b55920693db
mbajerwakaeric <erickmbanjerwaka@gmail.com>
2023-02-06 15:11:37 +0300

    add and remove to cart

--- ec/app/views.py
+++ ec/app/views.py
@@ -156,3 +156,23 @@ def minus_cart(request):
             'totalamount' :totalamount
         }
         return JsonResponse(data)
+
+def remove_cart(request):
+    if request.method == 'GET':
+        prod_id=request.GET['prod_id']
+        c = Cart.objects.get(Q(product=prod_id) & Q(user=request.user))
+        c.quantity+=1
+        c.delete()
+        user = request.user
+        cart = Cart.objects.filter(user=user)
+        amount = 0
+        for p in cart:
+            value = p.quantity * p.product.discounted_price
+            amount = amount + value
+        totalamount = amount + 40
+        data={
+            'quantity' :c.quantity,
+            'amount' :amount,
+            'totalamount' :totalamount
+        }
+        return JsonResponse(data)
