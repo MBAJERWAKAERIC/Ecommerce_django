8b5b93e902aa189541bbd7f6e2e6c2d1aed95bd4
mbajerwakaeric <erickmbanjerwaka@gmail.com>
2023-02-22 21:37:40 +0300

    hh

--- ec/app/views.py
+++ ec/app/views.py
@@ -118,7 +118,7 @@ def show_cart(request):
 
 class checkout(View):
     def get(self, request):
-        return render(request, "app/checkout.html",locals)
+        return render(request, 'app/checkout.html',locals())
 
 def plus_cart(request):
     if request.method == 'GET':
