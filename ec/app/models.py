9627f86d4f4784524de39cb9e84ea0d4f5a10c83
mbajerwakaeric <erickmbanjerwaka@gmail.com>
2023-02-24 13:47:28 +0300

    pay

--- ec/app/models.py
+++ ec/app/models.py
@@ -110,19 +110,20 @@ STATUS_CHOICES = (
 )
 
 class Payment(models.Model):
-    user = models.Foreignkey(user,on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
     amount = models.FloatField()
-    razorpay_order_id = models.CharField(max_lenght=100,blank=True,null=True)
-    razorpay_payment_status = models.CharField(max_lenght=100,blank=True,null=True)
-    razorpay_payment_id = models.CharField(max_lenght=100,blank=True,null=True)
+    razorpay_order_id = models.CharField(max_length=100,blank=True,null=True)
+    razorpay_payment_status = models.CharField(max_length=100,blank=True,null=True)
+    razorpay_payment_id = models.CharField(max_length=100,blank=True,null=True)
+    paid = models.BooleanField(default=False)
 
 class OrderPlaced(models.Model):
-    user = models.ForeignKey(UserUser,on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
     ordered_date = models.DateTimeField(auto_now_add=True)
-    status = models.Charfield(max_length=50,chices=STATUS_CHOICES, default='pending')
+    status = models.CharField(max_length=50,choices=STATUS_CHOICES, default='pending')
     payment = models.ForeignKey(Payment, on_delete=models.CASCADE,default="")
     @property
     def total_cost(self):
